79eb2ea9bdb34852665f900a44c04137c14c6af8
ruthie1246 <ruthyao308@gmail.com>
2018-07-24 15:45:26 -0500

    adds random colors and frequencies

--- simonsays.py
+++ simonsays.py
@@ -9,17 +9,37 @@ G_pin = 12
 B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
 
-def append_list():
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+buzz_pin = 32
+
+GPIO.setup(buzz_pin,GPIO.OUT)
+Buzz = GPIO.PWM(buzz_pin,1000)
+
+frequencies = [220, 440, 880, 1760]
+
+def loop():
+    n = random.randint(0,3)
+    color_sequence = [colors[n]]
+    frequency_sequence = [frequencies[n]]
     while True:
+        for i in range(0, len(color_sequence)):
+            Buzz.ChangeFrequency(frequency_sequence[i])            
+            Buzz.start(50)
+            LED.setColor(color_sequence[i])
+            time.sleep(0.5)
+            Buzz.stop()            
+            LED.noColor()
+            time.sleep(0.5)
         n = random.randint(0,3)
-        LED.setColor(colors[n])
+        color_sequence.append(colors[n])
+        frequency_sequence.append(frequencies[n])
         time.sleep(0.5)
-        LED.noColor()
-        time.sleep(0.5)
-        
+
 if __name__  == '__main__':
     try:
-        append_list()
+        loop()
     except KeyboardInterrupt:
         print 'Goodbye'
         LED.destroy()
